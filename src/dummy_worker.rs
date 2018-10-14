7ef1e53f437c7534f53b39f6a808f866c69f8bac
Philippe GASSMANN <philoops@gmail.com>
2018-10-14 17:52:31 +0200

    Play around basic multithreading

--- src/dummy_worker.rs
+++ src/dummy_worker.rs
@@ -6,9 +6,11 @@ use Context;
 
 pub fn launch_worker(ctx: Arc<Context>) {
     let counter = promhelpers::new_counter(&ctx, "dummy_ops_count", "Dummy operations count");
-
-    thread::spawn(move || loop {
-        counter.inc();
-        thread::sleep(Duration::from_millis(100));
-    });
+    for _i in 1..20 {
+        let cloned_counter = counter.clone();
+        thread::spawn(move || loop {
+            cloned_counter.inc();
+            thread::sleep(Duration::from_millis(10));
+        });
+    }
 }
