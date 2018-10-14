f8669ea480462e8f9447e7c60d8cc0dc476df85e
Philippe GASSMANN <philoops@gmail.com>
2018-10-14 18:11:05 +0200

    Remove useless Arc usage
    This method does not need to know how the caller handles the Context reference

--- src/dummy_worker.rs
+++ src/dummy_worker.rs
@@ -1,10 +1,9 @@
 use promhelpers;
-use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 use Context;
 
-pub fn launch_workers(ctx: Arc<Context>) {
+pub fn launch_workers(ctx: &Context) {
     let counter = promhelpers::new_counter(&ctx, "dummy_ops_count", "Dummy operations count");
     for _i in 1..20 {
         let cloned_counter = counter.clone();
